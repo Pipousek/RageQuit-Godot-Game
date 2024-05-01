79a7c7561add44ea82fcc2bd284f354fc80629a0
Petr Nagy <p_nagy@utb.cz>
2024-05-01 21:56:22 +0200

    fix: freeze after character get into gate

--- scripts/spikes.gd
+++ scripts/spikes.gd
@@ -4,8 +4,6 @@ func _on_body_entered(body):
 	call_deferred("_change_scene")
 
 func _change_scene():
-	# Retrieve the current scene's path
 	var scene_path = get_tree().current_scene.scene_file_path
 	print(scene_path)
-	# Use change_scene instead of change_scene_to_file
 	get_tree().change_scene_to_file(scene_path)
